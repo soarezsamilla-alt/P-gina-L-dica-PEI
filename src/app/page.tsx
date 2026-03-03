e926b2a3a34375178032ca4ea295f236a6124288
Samilla Soarez <soarezsamilla@gmail.com>
2026-03-03 23:10:26 +0000

    troque somente a cor do card da sessão Plano Pro para: #f2fbfc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -309,7 +309,7 @@ export default function Home() {
               </Card>
 
               {/* Plano Pro */}
-              <Card className="bg-white shadow-2xl flex flex-col border-2 border-primary ring-4 ring-primary/20 relative text-black">
+              <Card className="bg-[#f2fbfc] shadow-2xl flex flex-col border-2 border-primary ring-4 ring-primary/20 relative text-black">
                 <div className="absolute top-0 -translate-y-1/2 w-full flex justify-center">
                     <div className="bg-primary text-primary-foreground px-4 py-1 rounded-full text-sm font-semibold shadow-lg">
                         MAIS VENDIDO ⭐
