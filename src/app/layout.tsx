90e0895223f291eedec31bb95a2cdeecee148508
Samilla Soarez <soarezsamilla@gmail.com>
2026-03-08 18:08:15 +0000

    remova isso da pagina o pixel no facebook que esse aqui: 212461879160727

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,22 +22,6 @@ export default function RootLayout({
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '2124618791607277');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
               window.pixelId = "69a7971fd1e92e19b65bf740";
               var a = document.createElement("script");
               a.setAttribute("async", "");
@@ -56,15 +40,6 @@ export default function RootLayout({
         ></script>
       </head>
       <body className="font-body antialiased">
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=2124618791607277&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
         {children}
         <Toaster />
       </body>
