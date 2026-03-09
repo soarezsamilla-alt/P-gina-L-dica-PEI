47190510b54356ecbc913f3e5543ffb34b691f4f
Samilla Soarez <soarezsamilla@gmail.com>
2026-03-09 00:45:02 +0000

    adicione esse pixel do facebook: 1509782580494120

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,8 +37,27 @@ export default function RootLayout({
           data-utmify-prevent-subids
           strategy="afterInteractive"
         />
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1509782580494120');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className="font-body antialiased">
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1509782580494120&ev=PageView&noscript=1"/>`,
+          }}
+        />
         {children}
         <Toaster />
       </body>
