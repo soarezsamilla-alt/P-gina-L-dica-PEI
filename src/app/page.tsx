d3fa8d57cbe19320fb6a25025cd68227a55fd6e1
Samilla Soarez <soarezsamilla@gmail.com>
2026-03-11 23:13:52 +0000

    aumente esse texto para 15px (_for element <CardTitle>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -218,7 +218,7 @@ export default function Home() {
                       />
                     </CardContent>
                     <CardHeader className="flex-grow p-4">
-                      <CardTitle className="text-sm">{bonus.title}</CardTitle>
+                      <CardTitle className="text-[15px]">{bonus.title}</CardTitle>
                       <CardDescription className="text-xs mt-1">{bonus.description}</CardDescription>
                     </CardHeader>
                     <CardFooter className="flex-col justify-center p-4 pt-0">
@@ -560,4 +560,5 @@ export default function Home() {
     </div>
   );
 }
+    
     
